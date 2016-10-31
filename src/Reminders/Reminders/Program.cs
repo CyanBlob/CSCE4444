ec99e476f68d2a4b867bb69a4c9fcd0c8527df7e
Andrew <andrewt1993@yahoo.com>
2016-10-31 11:02:26 -0500

    Temp push, working on reminders

---
+++ src/Reminders/Reminders/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reminders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = DateTime.Now;
+            Reminder reminder = new Reminder(date.AddSeconds(60));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/Reminders/Reminders/Reminder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reminders
+{
+    public class Reminder
+    {
+        public Reminder(DateTime date)
+        {
+            Console.WriteLine("Creating reminder!");
+            Console.WriteLine("The date is: " + date.ToString());
+
+            while(DateTime.Now < date){};
+
+            Console.WriteLine("REMINDER!");
+        }
+
+    }
+}
