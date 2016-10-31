c3c09731dfae6eb8e9d6fa07565754eb26000e3b
Zac Taylor (Laptop) <monatrox@gmail.com>
2016-10-31 12:05:35 -0500

    Added notifications project to source.
    Very little functionality here. Will probably change this to make it
    significantly easier to make notifications on the fly. This basically
    just serves as more of a starting point for the notification system.

---
+++ src/Notifications/Notifications/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+
+namespace Notifications
+{
+    /// <summary>
+    /// Very simple notification thingy
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //There are a few different template toast notifications. This one happens to have 3 lines. 
+            XmlDocument toastXml = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText04);
+
+            XmlNodeList stringElements = toastXml.GetElementsByTagName("text");
+            stringElements[0].AppendChild(toastXml.CreateTextNode("Notifications Work!"));
+            stringElements[1].AppendChild(toastXml.CreateTextNode("The way I was trying before was stupid"));
+            stringElements[2].AppendChild(toastXml.CreateTextNode("This is literally less than 10 lines of code..."));
+            ToastNotification toast = new ToastNotification(toastXml);
+            ToastNotificationManager.CreateToastNotifier("MyAppID_0001").Show(toast);
+        }
+    }
+}
