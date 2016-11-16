2f4e0cc09439d30b3c738eb7c74c19e385ecb7cd
RogerTheJolly <blobsrule72@yahoo.com>
2016-11-15 21:35:26 -0600

    Added New Version of Youtube API

---
+++ src/YoutubeAPI/YoutubeAPI/NewAPI.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+
+namespace YouTubeAPI
+{
+    public class APICall
+    {
+        static void Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            string getSubsWithoutID = "https://www.googleapis.com/youtube/v3/subscriptions?part=snippet&mine=true&key=AIzaSyA-AZ4yYvaOPlGWb70p-V32n2StrmyFPiE";
+
+
+            var cookieJar = new CookieContainer();
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(getSubsWithoutID);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string Response = reader.ReadToEnd();
+            response.Close();
+            Console.Write(Response);
+        }
+    }
+}
