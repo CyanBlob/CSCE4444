1db7e1210abccd74071c7dcf28742147aed18d16
draftedvisions <danielm_tx@hotmail.com>
2016-12-06 17:52:10 -0600

    Trying to commit changes in small steps

---
+++ src/UIPrototype/Note.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+    namespace Weamy
+    {
+        namespace Notes
+        {
+            /// <summary>
+            ///  Class represents a single note (from various possible sources, or no sources...created in-app)
+            /// </summary>
+            public class Note
+            {
+                
+                private string _title;
+                private string _body;
+
+                /// <summary>
+                /// Default Constructor for Note
+                /// </summary>
+                public Note()
+                {
+                    _body = "";
+                    _title = "";
+                }
+
+                /// <summary>
+                /// Default Constructor for Note
+                /// </summary>
+                public Note(String title, String body)
+                {
+                    _body = body;
+                    _title = title;
+                }
+
+
+
+            /// <summary>
+            /// Get/Setter for private member _body
+            /// </summary>
+            /// <returns></returns>
+            public String body
+            {
+                get
+                {
+                    return this._body;
+                }
+                set
+                {
+                    this._body = value;
+                }
+            }
+
+            //    /// <summary>
+            //    /// Get/Setter for private member _title
+            //    /// </summary>
+            //    /// <returns></returns>
+            public String title
+            {
+                get
+                {
+                    return this._title;
+                }
+                set
+                {
+                    this._title = value;
+                }
+            }
+
+        }
+        }
+    }
+
---
+++ src/UIPrototype/NoteList.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Weamy
+{
+    namespace Notes
+    {
+        /// <summary>
+        /// A class to encapsulate a List of notes along with
+        /// Ancillary methods such as adding/removing notes
+        /// and loading data from file and moving to list
+        /// </summary>
+        public class NoteList
+        {
+            /// <summary>
+            /// Default Constructor initializes an empty list
+            /// </summary>
+            public NoteList()
+            {
+                notes = new List<Note> { };
+            }
+
+            /// <summary>
+            /// add note to list
+            /// </summary>
+            /// <param name="title">blah blip</param>
+            /// <param name="body"></param>
+            public void add(String title, String body)
+            {
+                Note newNote = new Note(title, body);
+                notes.Insert(0, newNote);
+            }
+            
+            /// <summary>
+            /// Clear notes list
+            /// And fill list with contents of local XML file
+            /// </summary>
+            /// <param name="fileName"></param>
+            public void loadFromFile(String fileName="notes.xml")
+            {
+                notes.Clear();
+                XmlSerializer cereal = new XmlSerializer(typeof(List<Note>));
+                FileStream fileStream = new FileStream(fileName, FileMode.Open);
+                notes = (List<Note>)cereal.Deserialize(fileStream);
+                fileStream.Close();
+            }
+
+
+            /// <summary>
+            /// Clear notes list
+            /// And fill list with contents of data file (XML???)
+            /// </summary>
+            /// <param name="fileName"></param>
+            public void saveToFile(String fileName="notes.xml")
+            {
+                XmlSerializer cereal = new XmlSerializer(typeof(List<Note>));
+                TextWriter fileStream = new StreamWriter(fileName);
+                cereal.Serialize(fileStream, notes);
+                fileStream.Close();
+            }
+
+            public static List<Note> notes;
+        }
+    }
+}
+
